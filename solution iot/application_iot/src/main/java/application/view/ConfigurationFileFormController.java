b753510e35bcef434f8943e69ac44fb71e446e3b
Victor Jockin <148845139+victorjockin@users.noreply.github.com>
2024-11-26 14:35:38 +0100

    ajout formulaire vide et controller de vue associé

---
+++ solution iot/application_iot/src/main/java/application/view/ConfigurationFileFormController.java
@@ -0,0 +1,15 @@
+package application.view ;
+
+/**
+ * Contrôleur de vue du formulaire de création
+ * d'un fichier de configuration.
+ * 
+ * Date de dernière configuration :
+ * - Mardi 18 novembre 2024 -
+ * 
+ * @author Victor Jockin (Équipe G2B12)
+ */
+public class ConfigurationFileFormController
+{
+    
+}
