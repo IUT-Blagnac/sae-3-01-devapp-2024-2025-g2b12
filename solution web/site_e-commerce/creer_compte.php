c41b50c02666ceb367e85264944d381fbdcec546
Victor Jockin <148845139+victorjockin@users.noreply.github.com>
2024-12-11 23:26:16 +0100

    Web : Mise en forme et styles formulaire de création de compte structure OK

--- solution web/site_e-commerce/creer_compte.php
+++ solution web/site_e-commerce/creer_compte.php
@@ -19,26 +19,42 @@
 
     <!-- formulaire de création de compte -->
     <!-- auteur : Victor Jockin -->
-    <?php echo "<br/><strong>-  En cours de développement  -</strong><br/><br/>" ; ?>
-    <div class="main-container">
+    <?php //echo "<br/><strong>-  En cours de développement  -</strong><br/><br/>" ; ?>
+    <div class="main-content">
         <div class="form-container">
-            <h1>Créer un compte</h1>
-            <form action='traitement_connexion.php' method='post'>
-                <label for='login'>Login:</label>
-                <input type='text' id='login' name='login' value='<?php echo $login; ?>' required><br><br>
-                <label for='email'>Email:</label>
-                <input type='email' id='email' name='email' value='<?php echo $email; ?>' required><br><br>
-                <label for='password'>Mot de passe:</label>
-                <input type='password' id='password' name='password' required><br><br>
-                <label for='nom'>Nom:</label>
-                <input type='text' id='nom' name='nom' value='<?php echo $nom; ?>' required><br><br>
-                <label for='prenom'>Prénom:</label>
-                <input type='text' id='prenom' name='prenom' value='<?php echo $prenom; ?>' required><br><br>
-                <label for='numTel'>Numéro de téléphone:</label>
-                <input type='text' id='numTel' name='numTel' value='<?php echo $numTel; ?>' required><br><br>
-                <input type='hidden' name='action' value='creer_compte'>
-                <input type='submit' value='Créer un compte'>
-            </form>
+            <div class="form-title">
+                <h1>Créer un compte</h1>
+            </div>
+            <div class="form-content">
+                <form action='traitement_connexion.php' method='post'>
+                    <div class="input-container">
+                        <label for='prenom' class="input-field-label">Prénom</label>
+                        <input type='text' id='prenom' name='prenom' value='<?php echo $prenom; ?>' class="input-field" required>
+                    </div>
+                    <div class="input-container">
+                        <label for='nom' class="input-field-label">Nom</label>
+                        <input type='text' id='nom' name='nom' value='<?php echo $nom; ?>' class="input-field" required>
+                    </div>
+                    <div class="input-container">
+                        <label for='login' class="input-field-label">Nom d'utilisateur (login)</label>
+                        <input type='text' id='login' name='login' value='<?php echo $login; ?>' class="input-field" required>
+                    </div>
+                    <div class="input-container">
+                        <label for='password' class="input-field-label">Mot de passe</label>
+                        <input type='password' id='password' name='password' class="input-field" required>
+                    </div>
+                    <div class="input-container">
+                        <label for='email' class="input-field-label">Email</label>
+                        <input type='email' id='email' name='email' value='<?php echo $email; ?>' class="input-field" required>
+                    </div>
+                    <div class="input-container">
+                        <label for='numTel' class="input-field-label">Numéro de téléphone</label>
+                        <input type='text' id='numTel' name='numTel' value='<?php echo $numTel; ?>' class="input-field" required>
+                    </div>
+                    <input type='hidden' name='action' value='creer_compte'>
+                    <input type='submit' value='Créer un compte'>
+                </form>
+            </div>
         </div>
     </div>
 </body>
