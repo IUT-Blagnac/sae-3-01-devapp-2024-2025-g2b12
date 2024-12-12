6e2f12f9e2deeda2b2f232f141b3b80937b03978
Victor Jockin <148845139+victorjockin@users.noreply.github.com>
2024-12-12 16:58:08 +0100

    fonts

--- solution web/site_e-commerce/creer_compte.php
+++ solution web/site_e-commerce/creer_compte.php
@@ -28,31 +28,31 @@
             <div class="form-content">
                 <form action='traitement_connexion.php' method='post'>
                     <div class="input-container">
-                        <label for='prenom' class="input-field-label">Prénom</label>
+                        <label class='input-field-label' for='prenom'>Prénom</label>
                         <input type='text' id='prenom' name='prenom' value='<?php echo $prenom; ?>' class="input-field" required>
                     </div>
                     <div class="input-container">
-                        <label for='nom' class="input-field-label">Nom</label>
+                        <label class='input-field-label' for='nom'>Nom</label>
                         <input type='text' id='nom' name='nom' value='<?php echo $nom; ?>' class="input-field" required>
                     </div>
                     <div class="input-container">
-                        <label for='login' class="input-field-label">Nom d'utilisateur (login)</label>
+                        <label class='input-field-label' for='login'>Nom d'utilisateur (login)</label>
                         <input type='text' id='login' name='login' value='<?php echo $login; ?>' class="input-field" required>
                     </div>
                     <div class="input-container">
-                        <label for='password' class="input-field-label">Mot de passe</label>
+                        <label class='input-field-label' for='password'>Mot de passe</label>
                         <input type='password' id='password' name='password' class="input-field" required>
                     </div>
                     <div class="input-container">
-                        <label for='email' class="input-field-label">Email</label>
+                        <label class='input-field-label' for='email'>Email</label>
                         <input type='email' id='email' name='email' value='<?php echo $email; ?>' class="input-field" required>
                     </div>
                     <div class="input-container">
-                        <label for='numTel' class="input-field-label">Numéro de téléphone</label>
+                        <label class='input-field-label' for='numTel'>Numéro de téléphone</label>
                         <input type='text' id='numTel' name='numTel' value='<?php echo $numTel; ?>' class="input-field" required>
                     </div>
-                    <input type='hidden' name='action' value='creer_compte'>
-                    <input type='submit' value='Créer un compte'>
+                    <!--<input type='hidden' name='action' value='creer_compte'>-->
+                    <input class='validation-button' type='submit' value="CRÉER">
                 </form>
             </div>
         </div>
