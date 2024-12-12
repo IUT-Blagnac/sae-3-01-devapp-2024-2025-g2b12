8492136a418e5a0ab144489f7400167a9c83a7a2
Hextaz <biblocquenolhan@gmail.com>
2024-12-12 13:15:19 +0100

    session.php

--- solution web/site_e-commerce/include/head.php
+++ solution web/site_e-commerce/include/head.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projet</title>
+    <title>Whoop Resell</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
